9e871680ee470eebde3c4a364825119aec49ced0
NhanVo <vothanhnhan.it@gmail.com>
2017-03-15 21:48:11 +0700

    Add files via upload
    First commit

---
+++ Working/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  	$("#top-strip").load("./top-strip/top-strip.html");
+    $("#top-wrapper").load("./top-wrapper/top-wrapper.html");
+    $("#navigation-wrapper").load("./navigation-wrapper/navigation-wrapper.html");
+    $("#breadcrumb").load("./breadcrumb/breadcrumb.html");
+});
